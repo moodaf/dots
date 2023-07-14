41f3b293a70305bcd14c5628308bc51dbb871bf7
moodaf <139394280+moodaf@users.noreply.github.com>
2023-07-14 16:47:55 +0000

    Add files via upload

---
+++ mountain/config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run go here
+    alias anime="anipy-cli"
+    alias youtube="ytfzf -t --notify-playing"
+    alias manga="manga-cli"
+    neofetch --kitty "/home/alex/Pictures/Wallpapers/snowcaped.png"
+end
+
+# Created by `pipx` on 2023-07-03 15:32:17
+set PATH $PATH /home/alex/.local/bin
