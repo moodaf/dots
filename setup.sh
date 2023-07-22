4d4b6dfae7cb49c911b9d1b5ce53bacb32adb7e5
moodaf <139394280+moodaf@users.noreply.github.com>
2023-07-22 20:12:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,58 +2,74 @@
 
 read -p "Do you want to proceed? (y/n): " choice
 if [[ $choice == "y" || $choice == "Y" ]]; then
-    echo "Proceeding..."  | pv -qL 25
+    echo "Proceeding..."
     
-    echo "Installing dependencies" | pv -qL 25
+    echo "Installing dependencies"
     sleep 3
     sudo pacman -S --needed git base-devel
     
-    echo "Installing yay aur helper" | pv -qL 25
+    echo "Installing yay aur helper"
     sleep 3
     cd
     sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
     
     
-    read -p "Which configuration would you like to install? ("mountain" is the only option for now): " rice
+    read -p "Which configuration would you like to install? (mountain/dynamic): " config
     
-    configuration=${configuration,,}
-    case $rice in
+    #stores name of user
+    me=$(whoami)
+    
+    case $config in
         mountain)
-            #moving config files and fonts to correct directories
-            cd ~/dots/mountain
-            me=$(whoami)
-            mkdir /home/$me/.config/
-            mv -f config/* /home/$me/.config/
-            mv -f fonts/* /home/$me/.local/share/fonts/
-            unzip themes.zip
-            cd /themes
-            mkdir /home/alex/$me/.themes
-            mv mountain ~/.themes
+            move_config_font_files mountain
+
+            #installing required packages
+            yay -S bspwm sxhkd rofi polybar dunst picom-jonaburg-git betterlockscreen kitty fish mate-polkit nitrogen neovim htop ly rofi-screenshot-git thunar lxappearance rofi-calc rofi-emoji zip unzip ttf-noto-nerd otf-firamono-nerd ttf-jetbrains-mono-nerd ttf-dejavu siji-ttf  ttf-unifont otf-unifont    
             
-            #bspwm requires that the "bspwm" be executable for it to work properly (otherwise a black screen is rendered)
-            chmod +x /home/$me/.config/bspwm/bpsmwrc
-            
-            #installing my disired packages
-            yay -S bspwm sxhkd rofi polybar dunst picom-jonaburg-git kitty fish mate-polkit nitrogen vscodium-bin neovim htop ly rofi-screenshot-git thunar lxappearance rofi-calc rofi-emoji zip unzip ttf-noto-nerd otf-firamono-nerd ttf-jetbrains-mono-nerd ttf-dejavu siji-ttf  ttf-unifont otf-unifont    
-            
-            #changing to ly display manager
-            display_manager=$(detect_display_manager)
-            if [[ $display_manager="unknown" || $display_manager="ly" ]]; then
-                sudo systemctl enable ly
-            else                
-                sudo systemctl display $display_manager
-                sudo systemctl enable ly
-            fi
+            #making "ly" the display manager
+            display_manager_changer ly
+
             #making fish default shell
-            echo "Making fish the default shell." | pv -qL 25
-            sleep 3
             chsh -s /bin/fish
-            echo "The script has been successful. Exiting..." | pv -qL 25
+            
+            echo "These changes will have an effect after a reboot."
+            read -p "Do you want to reboot now? (y/n): " yesorno
+            if [[ $yesorno == "y" || $yesorno == "Y" ]]; then
+                reboot
+            else
+                exit 0
+            fi
+
+            
             exit 0
             ;;
+        dynamic)            
+            move_config_font_files dynamic
+            #installing required packages
+            yay -S bspwm sxhkd rofi polybar dunst picom-jonaburg-git kitty fish mate-polkit nitrogen neovim htop ly rofi-screenshot-git thunar lxappearance rofi-calc rofi-emoji zip unzip ttf-noto-nerd otf-firamono-nerd ttf-jetbrains-mono-nerd ttf-dejavu siji-ttf  ttf-unifont otf-unifont
+            yay -S python-pywal python-pywalfox pywal-discord-git
+
+            #making "ly" the display manager
+            display_manager_changer ly
+
+            #making fish default shell
+            chsh -s /bin/fish
+            
+            echo "These changes will have an effect after a reboot."
+            read -p "Do you want to reboot now? (y/n): " yesorno
+            if [[ $yesorno == "y" || $yesorno == "Y" ]]; then
+                reboot
+            else
+                exit 0
+            fi
+
+
+
+
+
+            
         *)
-            echo "You have not chosen a valid configuration."
-            echo "Exiting..."
+            echo "You have not chosen a valid configuration. Exiting..."
             sleep 3
             exit 0
     esac
@@ -62,7 +78,23 @@ else
     exit 0
 fi
 
-#function detects the display manager present, obscure ones will be mistaken for not existing, however this is not really an issue as ones, other than these are extremely uncommon
+move_config_font_files() {
+    # "$1" is the name of the config folder, and should be specified when calling the function
+    cd ~/dots/$1
+    mkdir /home/$me/.config/
+    mv -f config/* /home/$me/.config/
+    mv -f fonts/* /home/$me/.local/share/fonts/
+    mkdir /home/$me/.themes
+    chmod +x /home/$me/.config/bspwm/bpsmwrc
+    mkdir /home/$me/Pictures/Wallpapers
+    mv wallpapers/* /home/$me/Pictures/Wallpapers
+    unzip themes.zip
+    mkdir /home/$me/.themes
+    mv /themes/* ~/.themes
+}
+
+#obscure display managers will be mistaken for not existing, however this is not really an issue as others are extremely uncommon
+#wont be a usecase where it would be necessary to detect other display managers so not worth coding
 detect_display_manager() {
     local display_manager
     
@@ -83,4 +115,15 @@ detect_display_manager() {
     fi
     
     echo $display_manager
+}
+
+#syntax: display_manager_changer (name of desired display manager)
+display_manager_changer() {
+    display_manager=$(detect_display_manager)
+    if [[ $display_manager="unknown" || $display_manager=$1 ]]; then
+        sudo systemctl enable $1
+    else                
+        sudo systemctl disable $display_manager
+        sudo systemctl enable $1
+    fi
 }
